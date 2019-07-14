1a74a48caf16ed6480c76a9bc1caa235940064bd
limingru <18813070947@163.com>
2019-07-14 11:34:49 +0800

    元组声明

---
+++ examples/section1/index.js
@@ -0,0 +1,7 @@
+// 数组的声明有两种
+var list1 = [1, 2, 2];
+var list2 = ["abc", "12"];
+// 元组的声明
+var x;
+x = ["a", 2, "b"];
+console.log(x.length);
--- examples/section1/index.ts
+++ examples/section1/index.ts
@@ -1,3 +1,8 @@
 // 数组的声明有两种
 let list1: number[] = [1, 2, 2];
 let list2: Array<string> = ["abc", "12"];
+
+// 元组的声明
+let x: [string, number, string];
+x = ["a", 2, "b"]
+console.log(x.length)
