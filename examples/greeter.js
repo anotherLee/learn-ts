eb508a8eb65795210e0f6d5a0af5476604ad8502
limingru <18813070947@163.com>
2019-07-14 11:03:20 +0800

    在例子中添加class语法例子

--- examples/greeter.js
+++ examples/greeter.js
@@ -1,7 +1,18 @@
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = this.firstName + " " + this.lastName;
+    }
+    return User;
+}());
 function greeter(person) {
     return "Hello " + person.firstName + person.lastName;
 }
-greeter({
+var user = {
     firstName: "Lee",
     lastName: "A"
-});
+};
+var user1 = new User("张", "三");
+greeter(user);
+greeter(user1);
